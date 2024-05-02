fa02ea613d5b0a3897eacacef3c48a8d5833bdd2
Satyajeet Srivastava <snera5015@gmail.com>
2024-05-02 18:37:26 +0530

    learned new concept

--- js/index.js
+++ js/index.js
@@ -1,6 +1,16 @@
-console.log("hello world"); 
-console.log("bhai success");
-console.log(5+3);
+// console.log("hello world"); 
+// console.log("bhai success");
+// console.log(5+3);
 alert("hello");
-var x = prompt("what is your name");
-alert("Hi " + x);
+var message= "Hello there, ";
+var name = prompt("what is your name");
+// give variable a meaningfull name 
+// basic rule for naming just like c\c++ and the camel casing rule for naming
+// string basic rule as c/c++ 
+alert(message + name);
+var text= prompt("you have total 10 character to use. \n write your message below");
+var leftchar = 10 - text.length;
+// alert("Total word in your name : " + name.length );
+
+alert("Total character used : "+ text.length + " \n Total character left : " + leftchar );
+alert(text.slice(0,10));
