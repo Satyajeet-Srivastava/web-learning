5925720581b042d9eef81c24a8519b2ea70407fd
Satyajeet Srivastava <snera5015@gmail.com>
2024-06-13 09:26:28 +0530

    done

--- js/index.js
+++ js/index.js
@@ -20,3 +20,62 @@ var remain=name.slice(1,20);
 remain=remain.toLowerCase();
 firstchar=firstchar.toUpperCase();
 alert(message + firstchar +remain );
+
+// learned about modulo use of barects in code
+
+
+var dog_age=prompt("what is your dog age :- ");
+// var human_age=prompt ("what is human age");
+var human_age=(dog_age-2)*4+21;
+console.log(human_age);
+
+// increment and decrement in s
+
+var a=10;
+a++;
+console.log(a);
+a--;
+console.log(a);
+
+//fuction in js
+
+// second type of funtion in js 
+
+function lifeInWeeks(age) {
+    
+  /************Don't change the code above************/    
+      
+      //Write your code here
+      var ages = 90-age;
+      var days = 365*ages;
+      var weeks = 52*ages;
+      var months =12*ages;
+      
+      console.log("You have "+days+" days, "+ weeks+" weeks, and "+months+" months left." )
+      
+  /*************Don't change the code below**********/
+  }
+  var age = prompt("what is your age:- " );
+  
+  lifeInWeeks(age)
+
+  //Create your function below this line.
+//The first parameter should be the weight and the second should be the height.
+function bmiCalculator(weight,height){
+  var bmi=weight/(height*height);
+  return Math.round(bmi);
+}
+
+
+
+//  If my weight is 65Kg and my height is 1.8m, I should be able to call your function like this:
+
+var bmi = bmiCalculator(65, 1.8); 
+console.log(bmi);
+
+// bmi should equal 20 when it's rounded to the nearest whole number.
+
+//begennier complete 
+// intermediate js start 
+
+
